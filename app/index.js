9c9cfdd010d28a211fa81ad0d9941201c154f124
Michael D. Perini <perinimichaeld@yahoo.com>
2019-12-01 19:21:10 -0500

    complete working sample

--- app/index.js
+++ app/index.js
@@ -1,54 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
+import React from 'react';
+import ReactDom from 'react-dom';
 
-/*
-    INSTRUCTIONS:
-    Create a "todo" app witht the follwing criteria:
-        1. The user can add new todo items.
-        2. The user can remove todo items.
-*/
+import './styles.css';
 
-const generatedId = () => '_' + Math.random().toString(36).substr(2,9);
+const CounterGame = () => {
+    const [count, setCount] = React.useState(0)
+    const [timeLeft, setTimeLeft] = React.useState(10)
+    const id = React.useRef(null)
 
-const Todo = () => {
-    const [todos, setTodos] = React.useState([])
-    const [input, setInput] = React.useState('')
+    const clear = () => window.clearInterval(id.current)
 
-    const handleSubmit = () => {
-        setTodos((todos)=> todos.concat(
-            {
-                text: input,
-                id: generatedId()
-            }))
-        setInput('')
-    }
+    React.useEffect(() => {
+        id.current = window.setInterval(() => {
+            setTimeLeft((time) => time - 1)
+        }, 1000)
 
+        return clear
+    }, [])
 
-const removeTodo = (id) => setTodos((todos) => todos.filter((t) => t.id !== id))
+    React.useEffect(() => {
+        if (timeLeft === 0) {
+            clear()
+        }
+    }, [timeLeft])
 
-return (
-    <div>
-        <input 
-         type='text'
-         value={input}
-         onChange={(e) => setInput(e.target.value)}
-         placeholder='New Todo'
-         />
-         
-         <button onClick={handleSubmit}>Submit</button>
-
-         <ui>
-           {todos.map(({ text, id}) => (
-               <li key={id}>
-                   <span>{text}</span>
-                   <button onClick={() => removeTodo(id)}>x</button>
-            </li>
-           ))}  
-         </ui>
-    </div>
+    return (
+        <div className="App">
+            <h1>{count}</h1>
+            <h3>Time Left: {timeLeft} seconds</h3>
+            {timeLeft === 0 ? null :
+                <button onClick={() => setCount((c) => c + 1)}>
+                    +
+                </button>}
+        </div>
     )
- }
+}
 
- const rootElement = document.getElementById('root');
- ReactDOM.render(<Todo />, rootElement);
+const rootElement = document.getElementById('root')
+ReactDom.render(<CounterGame />, rootElement)
